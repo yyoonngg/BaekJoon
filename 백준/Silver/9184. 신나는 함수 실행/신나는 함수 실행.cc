8a6e19b78a5fd0b4d46b65c91cf32fe1f872aee1
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-03-28 02:18:50 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 984 ms, Memory: 2636 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 987654321;
+int dp[54][54][54];
+
+int go(int a, int b, int c) {
+    if(a <= 0 || b <= 0 || c <= 0) return 1;
+    int &ret = dp[a][b][c];
+    if(ret != INF) return ret;
+
+    if(a > 20 || b > 20 || c > 20) return ret = go(20, 20, 20);
+
+    if(a < b && b < c) return ret = go(a, b, c - 1) + go(a, b - 1, c - 1) - go(a, b - 1, c);
+
+    return ret = go(a - 1, b, c) + go(a - 1, b - 1, c) + go(a - 1, b, c - 1) - go(a - 1, b - 1, c - 1);
+
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    while(true) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a == -1 && b == -1 && c == -1) break;
+        fill(&dp[0][0][0], &dp[0][0][0] + 54*54*54, INF);
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << go(a, b, c) << '\n';
+    }
+    return 0;
+}
