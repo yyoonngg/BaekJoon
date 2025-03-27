05703642e4732f799ad62e5d33d24184b548e1af
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-03-28 02:19:47 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2636 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -13,18 +13,16 @@ int go(int a, int b, int c) {
     if(a < b && b < c) return ret = go(a, b, c - 1) + go(a, b - 1, c - 1) - go(a, b - 1, c);
 
     return ret = go(a - 1, b, c) + go(a - 1, b - 1, c) + go(a - 1, b, c - 1) - go(a - 1, b - 1, c - 1);
-
-    return 0;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    fill(&dp[0][0][0], &dp[0][0][0] + 54*54*54, INF);
     while(true) {
         int a, b, c;
         cin >> a >> b >> c;
         if(a == -1 && b == -1 && c == -1) break;
-        fill(&dp[0][0][0], &dp[0][0][0] + 54*54*54, INF);
         cout << "w(" << a << ", " << b << ", " << c << ") = " << go(a, b, c) << '\n';
     }
     return 0;
