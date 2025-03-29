a4debed2a01818bf2763b4b2702d5a1a3998e019
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-03-29 16:58:40 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -10,18 +10,13 @@ int main() {
         cin >> t[i] >> p[i];
     }
 
-    // DP 배열 초기화 (안 하면 쓰레기 값이 들어갈 수도 있음)
-    memset(dp, 0, sizeof(dp));
-
     // 뒤에서부터 DP 계산
     for (int i = n; i >= 1; i--) {
         int nextDay = i + t[i];  // 현재 상담을 끝낸 다음 날
-
         if (nextDay > n + 1) {   // 퇴사일을 넘어가면 상담 불가능
             dp[i] = dp[i + 1]; 
         } else {
-            // 상담을 안 하는 경우 vs 상담을 하는 경우 중 최댓값 선택
-            dp[i] = max(dp[i + 1], p[i] + (nextDay <= n ? dp[nextDay] : 0));
+            dp[i] = max(dp[i + 1], p[i] + dp[nextDay]);
         }
     }
 
