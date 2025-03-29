f2fecc5ee6f4a3c353fe6bedb6d12c00f8f2623b
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-03-29 16:59:57 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,25 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int n;
-int t[16], p[16], dp[16];
+int n, dp[20], t[20], p[20];
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for(int i = 1; i <= n; i++) {
         cin >> t[i] >> p[i];
     }
 
-    // 뒤에서부터 DP 계산
-    for (int i = n; i >= 1; i--) {
-        int nextDay = i + t[i];  // 현재 상담을 끝낸 다음 날
-        if (nextDay > n + 1) {   // 퇴사일을 넘어가면 상담 불가능
-            dp[i] = dp[i + 1]; 
-        } else {
-            dp[i] = max(dp[i + 1], p[i] + dp[nextDay]);
+    for(int i = n; i >= 1; i--) {
+        int next = i + t[i];
+        if(next > n + 1) {
+            dp[i] = dp[i + 1];
+        }
+        else {
+            dp[i] = max(dp[i + 1], p[i] + dp[next]);
         }
     }
 
-    cout << dp[1] << "\n";  // 1일부터 시작했을 때 최대 이익 출력
+    cout << dp[1] << "\n";
+
     return 0;
 }
