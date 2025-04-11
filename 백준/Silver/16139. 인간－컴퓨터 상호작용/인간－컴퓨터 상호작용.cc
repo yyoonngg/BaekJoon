795d7c6d16c794369e632af2e142667b02785482
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-04-11 23:27:13 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 0 ms, Memory: 2260 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int q, l, r, psum[2004][30];
+char a;
+string s;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> s >> q;
+    for(int i = 0; i < (int)s.size(); i++) {
+        int alpha = (int)(s[i] - 'a');
+        for(int j = 0; j < 26; j++) {
+            if(j == alpha) {
+                psum[i + 1][j] = psum[i][j] + 1;
+            }
+            else {
+                psum[i + 1][j] = psum[i][j];
+            }
+        }
+    }
+    while(q--) {
+        cin >> a >> l >> r;
+        l++; r++;
+        int alpha = (int)(a - 'a');
+        cout << psum[r][alpha] - psum[l - 1][alpha] << "\n";
+    }
+
+    return 0;
+}
