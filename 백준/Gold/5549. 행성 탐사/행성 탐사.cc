7013e102c6b518040e6e324c64ee6b6821b1e178
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-04-15 23:04:00 +0900

    [Gold V] Title: 행성 탐사, Time: 68 ms, Memory: 13640 KB -BaekjoonHub

--- 백준/Gold/5549. 행성 탐사/행성 탐사.cc
+++ 백준/Gold/5549. 행성 탐사/행성 탐사.cc
@@ -1,52 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int n, m, k, psum[1004][1004][3];
-char a[1004][1004];
-struct A {
-    int a, b, c, d;
-};
-vector<A> v;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> m >> n >> k;
-    for(int i = 1; i <= m; i++) {
-        for(int j = 1; j <= n; j++) {
-            cin >> a[i][j];
-            if(a[i][j] == 'J') {
-                psum[i][j][0] = psum[i][j - 1][0] + 1; 
-                psum[i][j][1] = psum[i][j - 1][1]; 
-                psum[i][j][2] = psum[i][j - 1][2]; 
-            }
-            else if(a[i][j] == 'O') {
-                psum[i][j][0] = psum[i][j - 1][0]; 
-                psum[i][j][1] = psum[i][j - 1][1] + 1; 
-                psum[i][j][2] = psum[i][j - 1][2]; 
-            }
-            else if(a[i][j] == 'I') {
-                psum[i][j][0] = psum[i][j - 1][0]; 
-                psum[i][j][1] = psum[i][j - 1][1]; 
-                psum[i][j][2] = psum[i][j - 1][2] + 1; 
-            }
-        }
-    }
-    for(int i = 0; i < k; i++) {
-        int ta, tb, tc, td;
-        cin >> ta >> tb >> tc >> td;
-        v.push_back({ta, tb, tc, td});
-    }
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    for(int i = 0; i < k; i++) {
-        A here = v[i];
-        int jSum = 0, oSum = 0, iSum = 0;
-        for(int j = here.a; j <= here.c; j++) {
-            jSum += psum[j][here.d][0] - psum[j][here.b - 1][0]; 
-            oSum += psum[j][here.d][1] - psum[j][here.b - 1][1]; 
-            iSum += psum[j][here.d][2] - psum[j][here.b - 1][2]; 
+    int M, N, K;
+    cin >> M >> N >> K;
+    
+    // 1-indexed 배열로 사용 (인덱스 0은 0으로 채워짐)
+    const int MAX_M = 1001, MAX_N = 1001;
+    int psumJ[MAX_M][MAX_N] = {0};
+    int psumO[MAX_M][MAX_N] = {0};
+    int psumI[MAX_M][MAX_N] = {0};
+
+    // 지도 입력 및 2차원 누적합 배열 계산
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= N; j++) {
+            char c;
+            cin >> c;
+            psumJ[i][j] = psumJ[i-1][j] + psumJ[i][j-1] - psumJ[i-1][j-1] + (c == 'J');
+            psumO[i][j] = psumO[i-1][j] + psumO[i][j-1] - psumO[i-1][j-1] + (c == 'O');
+            psumI[i][j] = psumI[i-1][j] + psumI[i][j-1] - psumI[i-1][j-1] + (c == 'I');
         }
-        cout << jSum << " " << oSum << " " << iSum << "\n";
     }
-    // 정글 J, 바다 O, 얼음 I
+    
+    // 각 쿼리에 대해 영역 내 각 지형의 개수를 O(1)에 계산
+    while(K--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int cntJ = psumJ[c][d] - psumJ[a-1][d] - psumJ[c][b-1] + psumJ[a-1][b-1];
+        int cntO = psumO[c][d] - psumO[a-1][d] - psumO[c][b-1] + psumO[a-1][b-1];
+        int cntI = psumI[c][d] - psumI[a-1][d] - psumI[c][b-1] + psumI[a-1][b-1];
+        cout << cntJ << " " << cntO << " " << cntI << "\n";
+    }
+    
     return 0;
 }
