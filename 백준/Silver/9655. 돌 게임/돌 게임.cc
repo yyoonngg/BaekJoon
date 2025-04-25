d5a1b1e55aacbeb7eb1dce477078a51a05940dae
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-04-25 21:02:52 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,19 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, dp[1004], turn;
+
+int n;
+bool dp[1004]; // true면 상근 승리, false면 창영 승리
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n; 
-    fill(dp, dp + 1004, -1);
-    for(int i = 0; i < n; i++) {
-        if(dp[i] != -1) continue;
-        dp[i] = turn;
-        dp[i + 2] = turn;
-        turn ^= 1;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+
+    dp[0] = false; // 돌이 없으면 상근이 패배
+    for (int i = 1; i <= n; i++) {
+        // 돌 1개 또는 3개 가져갔을 때 상대방이 패배하는 경우가 있으면, 이김
+        if (i - 1 >= 0 && !dp[i - 1]) dp[i] = true;
+        else if (i - 3 >= 0 && !dp[i - 3]) dp[i] = true;
+        else dp[i] = false; // 둘 다 아니면 패배
     }
-    if(dp[n - 1] == 0) cout << "SK\n";
-    else cout << "CY\n";
-    return 0;
+
+    cout << (dp[n] ? "SK" : "CY") << "\n";
 }
