3f851235665ba3a8d5f505e187c94d9b4cca43f1
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-05-01 17:19:29 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -12,45 +12,24 @@ int main() {
         q.push(stu);
     }
 
-    while(true) {
-        if(q.empty() && stk.empty()) break;
-
-        int q_here, stk_here;
-
-        if(!q.empty()) {
-            q_here = q.front();
-            if(q_here == turn) {
-                q.pop();
-                turn++;
-                continue;
-            }
-            else {
-                if(!stk.empty() && stk.top() == turn) {
-                    stk.pop();
-                    turn++;
-                    continue;
-                }
-                else {
-                    stk.push(q_here);
-                    q.pop();
-                    continue;
-                }
-            }
+    while(!q.empty() || !stk.empty()) {
+        if(!q.empty() && q.front() == turn) {
+            q.pop();
+            turn++;
+        }
+        else if(!stk.empty() && stk.top() == turn) {
+            stk.pop();
+            turn++;
+        }
+        else if(!q.empty()) {
+            stk.push(q.front());
+            q.pop();
         }
         else {
-            stk_here = stk.top();
-            if(stk_here == turn) {
-                stk.pop();
-                turn++;
-                continue;
-            }
-            else {
-                cout << "Sad\n";
-                return 0;
-            }
+            cout << "Sad\n";
+            return 0;
         }
     }
-
     cout << "Nice\n";
 
     return 0;
