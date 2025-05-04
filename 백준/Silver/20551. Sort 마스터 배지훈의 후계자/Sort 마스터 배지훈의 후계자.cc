180ba4b4eac351b0b3efed5f17505a3879cce290
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-05-04 10:08:45 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 104 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.cc
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.cc
@@ -1,28 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a[200004];
-map<int, int> mp;
+int n, m;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
+    
+    vector<int> a(n);
+
     for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    sort(a, a + n);
-    for(int i = 0; i < n; i++) {
-        if(!mp[a[i]]) mp[a[i]] = i + 1;
-    }
+
+    sort(a.begin(), a.end());
+
     for(int i = 0; i < m; i++) {
         int d;
         cin >> d;
-        if(mp[d]) {
-            cout << mp[d] - 1 << "\n";
+
+        int left = 0, right = n - 1;
+        int ans = -1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(a[mid] < d) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        if(left < n && a[left] == d) {
+            cout << left << "\n";
         }
         else {
             cout << "-1\n";
-        }  
+        }
     }
     return 0;
 }
