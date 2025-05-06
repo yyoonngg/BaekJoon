28d8f5e94b827083d2b3ec321f4d247e6180e839
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-05-06 23:25:33 +0900

    [Silver II] Title: N과 M (9), Time: 20 ms, Memory: 4312 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, a[10004];
+vector<int> v;
+bool used[8];
+set<vector<int>> ret;
+
+void backtrack() {
+    if((int)v.size() == m) {
+        ret.insert(v);
+        return;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(used[i]) continue;
+
+        used[i] = true;        
+        v.push_back(a[i]);
+        backtrack();
+        v.pop_back();
+        used[i] = false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    backtrack();
+
+    for(auto _ret: ret) {
+        for(int x : _ret) cout << x << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
+
