e88c3607f82f7d654e15032ad25af8077730268f
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-05-18 18:01:52 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int cnt[26];
+
+int main() {
+    cin >> s;
+
+    for(int i = 0; i < s.size(); i++) {
+        cnt[s[i]-'a']++;
+    }
+    
+    for(int i = 0; i < 26; i++) cout << cnt[i] << " ";
+    return 0;
+}
