fae5ef65ba078d75d9032eb7fbd58273343d6afd
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-05-24 01:33:11 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,54 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define y1 aaaa
-
-// 모음(a,e,i,o,u) 하나를 반드시 포함하여야 한다.
-// 모음이 3개 혹은 자음이 3개 연속으로 오면 안 된다.
-// 같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
-
 string s;
-const char vowel[] = {'a', 'e', 'i', 'o', 'u'};
-
-bool isVowel(char c){
-    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
-    else return false;
+int lcnt, vcnt;
+bool isVowel(int idx) {
+    return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
-    while(1) {
-        cin >> s;
-        if(s == "end") return 0;
-        
-        bool isvcnt = false;
-        int vcnt = 0;
-        int ocnt = 0;
-        char tmp = 'A';
-
-        for(int i = 0; i < s.size(); i++) {
-            if(isVowel(s[i])) { isvcnt=true; vcnt++; ocnt=0; }
-            else { ocnt++; vcnt=0; }
-
-            if(ocnt == 3 || vcnt == 3) {
-                isvcnt = false; 
-                break;
-            }
-            if(s[i] == tmp && tmp != 'e' && tmp != 'o') {
-                isvcnt = false;
-                break;
-            }
-            tmp = s[i];
+    while(cin >> s) {
+        if(s == "end") break;
+        lcnt = vcnt = 0;
+        bool flag = 0;
+        bool is_include_v = 0;
+        int prev = -1;
+        for(int i = 0; i < (int)s.size(); i++) {
+            int idx = s[i];
+            if(isVowel(idx)) vcnt++, lcnt = 0, is_include_v = 1;
+            else vcnt = 0, lcnt++;
+            if(vcnt == 3 || lcnt == 3) flag = 1;
+            if(i >= 1 && (prev == idx) && (idx != 'e' && idx != 'o')) flag = 1;
+            prev = idx;
         }
-
-        if(isvcnt) {
-            cout << "<" << s << "> is acceptable.\n";
-        }
-        else {
-            cout << "<" << s << "> is not acceptable.\n";
-        }
+        if(is_include_v == 0) flag = 1;
+        if(flag) cout << "<" << s << "> is not acceptable.\n";
+        else cout << "<" << s << "> is acceptable.\n";
     }
-
     return 0;
 }
