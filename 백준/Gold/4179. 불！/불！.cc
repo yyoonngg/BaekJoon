4a7fd9857d42357d0749e7e31a32f45ca7feaf46
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-05-28 00:27:37 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 11016 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,68 +1,73 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-const int INF = 987654321;
-char a[1004][1004];
-int n,m,sx,sy,ret,y,x;
-int fire_check[1004][1004], person_check[1004][1004];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-
-bool in(int a, int b) {
-    return 0 <= a && a < n && 0 <= b && b < m;
-}
+int r, c, fire_visited[1004][1004], jihun_visited[1004][1004], ret;
+char a[1004][1004];
+queue<pair<int, int>> fire, jihun;
 
 int main() {
-    ios_base::sync_with_stdio(NULL);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
-    queue<pair<int, int>> q;
-    cin >> n >> m;
-    fill(&fire_check[0][0], &fire_check[0][0] + 1004 * 1004, INF);
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
+    cin >> r >> c;
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
             cin >> a[i][j];
             if(a[i][j] == 'F') {
-                fire_check[i][j] = 1; q.push({i, j});
+                fire.push({i, j});
+                fire_visited[i][j] = 1;
             }
             else if(a[i][j] == 'J') {
-                sy = i; sx = j;
+                jihun.push({i, j});
+                jihun_visited[i][j] = 1;
             }
         }
     }
 
-    while(q.size()) {
-        tie(y, x) = q.front(); q.pop();
-        for(int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(!in(ny, nx)) continue;
-            if(fire_check[ny][nx] != INF || a[ny][nx] == '#') continue;
-            fire_check[ny][nx] = fire_check[y][x] + 1;
-            q.push({ny, nx});
+    while(!jihun.empty()) {
+        ret++;
+
+        int fire_size = fire.size();
+        while(fire_size--) {
+            int y = fire.front().first;
+            int x = fire.front().second;
+            fire.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+                if (fire_visited[ny][nx] || a[ny][nx] == '#') continue;
+
+                fire_visited[ny][nx] = 1;
+                a[ny][nx] = 'F';
+                fire.push({ny, nx});
+            }
+        }
+
+        int jihun_size = jihun.size();
+        while(jihun_size--) {
+            int y = jihun.front().first;
+            int x = jihun.front().second;
+            jihun.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (ny < 0 || nx < 0 || ny >= r || nx >= c) {
+                    cout << ret << "\n";
+                    return 0;
+                }
+
+                if (jihun_visited[ny][nx] || a[ny][nx] == '#' || a[ny][nx] == 'F') continue;
+
+                jihun_visited[ny][nx] = 1;
+                jihun.push({ny, nx});
+            }
         }
     }
 
-    person_check[sy][sx] = 1;
-    q.push({sy, sx});
-    while(q.size()) {
-        tie(y, x) = q.front(); q.pop();
-        if(x == m - 1 || y == n - 1 || x == 0 || y == 0) {
-            ret = person_check[y][x];
-            break;
-        }
-
-        for(int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(!in(ny, nx)) continue;
-            if(person_check[ny][nx] || a[ny][nx] == '#') continue;
-            if(fire_check[ny][nx] <= person_check[y][x] + 1) continue;
-            person_check[ny][nx] = person_check[y][x] + 1;
-            q.push({ny, nx});
-        }
-    }
-
-    if(ret != 0) cout << ret << "\n";
-	else cout << "IMPOSSIBLE \n";   
+    cout << "IMPOSSIBLE\n";
     return 0;
 }
