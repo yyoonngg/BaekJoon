8b35d4ba90d0ea3a974ae31a8267598376e38111
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-06-02 17:31:52 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, ret;
-vector<pair<int, int>> v;
+vector<int> v;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -11,15 +11,15 @@ int main() {
     for(int i = 0; i < n; i++) {
         int num;
         cin >> num;
-        v.push_back({num, i});
+        v.push_back(num);
     }
 
     sort(v.begin(), v.end());
 
     int sum = 0;
-    for(auto vv: v) {
-        ret += (sum + vv.first);
-        sum += vv.first;
+    for(int vv: v) {
+        ret += (sum + vv);
+        sum += vv;
     }
     cout << ret << "\n";
     return 0;   
