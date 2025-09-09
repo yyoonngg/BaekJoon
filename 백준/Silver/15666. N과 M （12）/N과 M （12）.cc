ce33f86e7cc1abaa8b9e24fa667c3efe42f19100
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-09-09 23:07:52 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M, visited[10004], NN;
+int N, M;
 vector<int> a, v;
 
-void go(int n, int num) {
-    if(num == M) {
-        for(int nn : v) cout << a[nn] << " ";
+void go(int start, int depth) {
+    if(depth == M) {
+        for(int x : v) cout << x << " ";
         cout << "\n";
         return;
     }
 
-    for(int i = n; i < NN; i++) {
-        v.push_back(i);
-        go(i, num + 1);
+    for(int i = start; i < (int)a.size(); i++) {
+        v.push_back(a[i]);
+        go(i, depth + 1);
         v.pop_back();    
     }
 }
@@ -20,24 +20,13 @@ void go(int n, int num) {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
     cin >> N >> M;
-    for(int i = 0; i < N; i++) {
-        int tmp;
-        cin >> tmp;
-        if(!visited[tmp]) {
-            a.push_back(tmp);
-            visited[tmp]++;
-        }
-    }
+    a.resize(N);
 
+    for(int i = 0; i < N; i++) cin >> a[i];
     sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
 
-    NN = a.size();
-
-    for(int i = 0; i < NN; i++) {
-        v.push_back(i);
-        go(i, 1);
-        v.pop_back();
-    }
-
+    go(0, 0);
 }
