92e2f3e37044ce031bbdbf1286ba997c218b19b8
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-09-11 15:34:34 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2504 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -1,38 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[10004][3];
+int dp[10004][4];
 int t, n;
 
 int go(int num, int m) {
     if(num == 0) return 1;
-    if(num < 0) return 0;
+    if(num < 0 || m == 0) return 0;
 
-    int _ret = 0;
+    int &ret = dp[num][m];
+    if(ret != -1) return ret;
 
-    for(int i = 1; i <= m; i++) {
-        if(i != m && (num - m) == 0) continue;
-        int &ret = dp[num - m][i];
-        if(ret != -1) _ret += ret;
-        else ret = go(num - m, i), _ret += ret;
-    }
-
-    return _ret;
+    ret = go(num, m - 1) + go(num - m, m);
+    return ret;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> t;
-    fill(&dp[0][0], &dp[0][0] + 10004 * 3, -1);
-    dp[0][1] = 1; dp[0][1] = 1; dp[0][1] = 1;
+
+    memset(dp, -1, sizeof(dp));
 
     while(t--) {
         cin >> n;
-        int sum = 0;
-
-        for(int i = 1; i <= 3; i++) {
-            sum += go(n, i);
-        }
-        cout << sum << "\n";
+        cout << go(n, 3) << "\n";
     }    
 }
