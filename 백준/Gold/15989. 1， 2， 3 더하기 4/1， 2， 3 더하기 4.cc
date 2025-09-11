7cf4bf51c9c5f3f33d7050028c705d5682cae2e5
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-09-11 15:41:45 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -1,28 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[10004][4];
-int t, n;
-
-int go(int num, int m) {
-    if(num == 0) return 1;
-    if(num < 0 || m == 0) return 0;
-
-    int &ret = dp[num][m];
-    if(ret != -1) return ret;
-
-    ret = go(num, m - 1) + go(num - m, m);
-    return ret;
-}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    memset(dp, -1, sizeof(dp));
+    int T;
+    cin >> T;
+    vector<int> qs(T);
+    int maxN = 0;
+    for (int i = 0; i < T; ++i) {
+        cin >> qs[i];
+        maxN = max(maxN, qs[i]);
+    }
 
-    while(t--) {
-        cin >> n;
-        cout << go(n, 3) << "\n";
-    }    
+    const int K = 3;
+    // ways[n][k] = 1..k만 사용해 n을 만드는 방법 수 (순서 무시)
+    vector<array<long long, K + 1>> ways(maxN + 1, {0,0,0,0});
+
+    // 0을 만드는 방법: 아무 것도 안 쓰기 -> 1가지
+    for (int k = 1; k <= K; ++k) ways[0][k] = 1;
+
+    for (int n = 1; n <= maxN; ++n) {
+        // k = 1: 1만 사용 가능 -> 항상 1가지 (전부 1로 채우기)
+        ways[n][1] = 1;
+
+        // k = 2: 2를 쓰지 않는 경우(=ways[n][1]) + 2를 적어도 한 번 쓰는 경우(=ways[n-2][2])
+        ways[n][2] = ways[n][1] + (n >= 2 ? ways[n - 2][2] : 0);
+
+        // k = 3: 3을 쓰지 않는 경우(=ways[n][2]) + 3을 적어도 한 번 쓰는 경우(=ways[n-3][3])
+        ways[n][3] = ways[n][2] + (n >= 3 ? ways[n - 3][3] : 0);
+    }
+
+    for (int n : qs) {
+        cout << ways[n][3] << '\n';
+    }
+    return 0;
 }
