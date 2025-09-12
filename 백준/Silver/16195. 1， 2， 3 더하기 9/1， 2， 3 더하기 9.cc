0290967b97858fe231f8f28be858d681a3f53d29
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-09-13 02:03:28 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 8 ms, Memory: 9896 KB -BaekjoonHub

---
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll MOD = 1000000009;
+ll t, n, m;
+ll dp[1004][1004];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> t;
+    
+    dp[0][0] = 1;
+    for(int i = 1; i <= 1000; i++) {
+        for(int j = 1; j <= i; j++) {
+            dp[i][j] = ((i >= 3 ? dp[i-3][j-1] % MOD: 0) + (i >= 2 ? dp[i-2][j-1] % MOD : 0) + dp[i-1][j-1] % MOD) % MOD;
+        }
+    }
+
+    while(t--) {
+        cin >> n >> m;
+        ll sum = 0;
+        for(int i = 1; i <= m; i++) {
+            sum = (sum + dp[n][i]) % MOD;
+        }
+        cout << sum << "\n";
+    }
+}
