1af8b9bf34c92fe773f6c8f16540b1d55343b0e1
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-09-18 19:37:32 +0900

    [Gold III] Title: 세 용액, Time: 48 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.cc
+++ 백준/Gold/2473. 세 용액/세 용액.cc
@@ -1,47 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll n, a[100004], l, r, ret = 9876543210;
-vector<ll> ans;
+using ll = long long;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int n;
     cin >> n;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+    vector<ll> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    sort(a.begin(), a.end());
 
-    sort(a, a + n);
-    
-    for(int here = 0; here < n; here++) {
-        int l = 0, r = n - 1;
-        if(l == here) l++;
-        if(r == here) r--;
+    ll best_sum = LLONG_MAX; // 최적 합(부호 포함)
+    ll x = 0, y = 0, z = 0;  // 정답 트리플
 
-        while(l < r) {
-            ll sum = a[here] + a[l] + a[r];
-            if(abs(sum) < abs(ret)) {
-                ret = sum;
-                ans.clear();
-                ans.push_back(a[here]);
-                ans.push_back(a[l]);
-                ans.push_back(a[r]);
+    for (int i = 0; i < n - 2; ++i) {
+        int l = i + 1, r = n - 1;
+        while (l < r) {
+            ll s = a[i] + a[l] + a[r];
+            if (llabs(s) < llabs(best_sum)) {
+                best_sum = s;
+                x = a[i]; y = a[l]; z = a[r];
+                if (best_sum == 0) { // 최적
+                    // 더 나아질 수 없으므로 바로 종료해도 OK
+                }
             }
-
-            if(sum < 0) {
-                l++;
-                if(l == here) l++;
-            }
-            else if(sum >= 0) {
-                r--;
-                if(r == here) r--;
-            }
+            if (s < 0) ++l;
+            else --r;
         }
+        if (best_sum == 0) break; // 전역 종료(선택)
     }
 
+    vector<ll> ans = {x, y, z};
     sort(ans.begin(), ans.end());
-
-    for(ll a : ans) cout << a << " ";
+    cout << ans[0] << ' ' << ans[1] << ' ' << ans[2] << '\n';
+    return 0;
 }
