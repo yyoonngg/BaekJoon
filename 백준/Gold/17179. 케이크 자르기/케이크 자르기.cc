627ce13260ab4e05249d6b6473b4750b476563b7
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-09-20 15:18:59 +0900

    [Gold IV] Title: 케이크 자르기, Time: 32 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17179. 케이크 자르기/케이크 자르기.cc
+++ 백준/Gold/17179. 케이크 자르기/케이크 자르기.cc
@@ -1,43 +1,43 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int n, m, l, s[1004], q[1004];
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+    cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m >> l;
-    for (int i = 0; i < m; i++) cin >> s[i];
-    for (int i = 0; i < n; i++) cin >> q[i];
+    for(int i = 0; i < m; i++) {
+        cin >> s[i];
+    }    
+    for(int i = 0; i < n; i++) {
+        cin >> q[i];
+    }
 
-    for (int t = 0; t < n; t++) {
-        int low = 1, high = l;      // mid=0은 항상 참이므로 1부터 시작
+    for(int t = 0; t < n; t++) {
+        int low = 1, high = l;
         int ans = 0;
 
-        while (low <= high) {
+        while(low <= high) {
             int mid = (low + high) / 2;
+            int last = 0, cnt = 0;
 
-            int last = 0;
-            int pieces = 0;         // "조각 수"를 센다 (길이 >= mid 인 조각만)
-
-            for (int i = 0; i < m; i++) {
-                if (s[i] - last >= mid) {
-                    pieces++;
+            for(int i = 0; i < m; i++) {
+                if((s[i] - last) >= mid) {
+                    cnt++;
                     last = s[i];
                 }
             }
-            if (l - last >= mid) pieces++;   // 마지막 조각도 mid 기준으로만 포함
+            if((l - last) >= mid) cnt++;
 
-            if (pieces >= q[t] + 1) {
-                // mid 길이로 Q번 잘라 Q+1조각(모두 mid 이상) 가능 → 더 크게 도전
-                ans = mid;          // 정답 후보는 mid 자체
+            if(cnt >= q[t] + 1) {
+                ans = mid;
                 low = mid + 1;
-            } else {
+            }
+            else {
                 high = mid - 1;
             }
         }
-
-        cout << ans << '\n';
+        cout << ans << "\n";
     }
+
 }
