90234c9e47e19dc3df774fdf121e39a16eba9458
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-11-10 02:17:14 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 0 ms, Memory: 2592 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -1,44 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-string s, ret;
-stack<char> stk;
 
-void make() {
-    while(!stk.empty()) {
-        char tmp = stk.top(); stk.pop();
-        ret += tmp;
-    }
-}
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s, ret, buffer;
     getline(cin, s);
-    
-    bool pass = false;
-    for(int i = 0; i < (int)s.size(); i++) {
-        if(s[i] == '<') {
-            pass = true;
-            make();
-        }
-        else if(s[i] == '>') {
-            pass = false;
-            ret += s[i];
-            continue;
-        }
 
-        if(pass) {
-            ret += s[i];
-            continue;
-        }
+    bool in_tag = false;
 
-        if(s[i] == ' ') {
-            make();
-            ret += ' ';
+    for (char c : s) {
+        if (c == '<') {
+            // 단어가 끝났다면 먼저 뒤집어서 추가
+            if (!buffer.empty()) {
+                reverse(buffer.begin(), buffer.end());
+                ret += buffer;
+                buffer.clear();
+            }
+            in_tag = true;
+            ret += c;
+        }
+        else if (c == '>') {
+            in_tag = false;
+            ret += c;
+        }
+        else if (in_tag) {
+            // 태그 내부는 그대로 출력
+            ret += c;
+        }
+        else if (c == ' ') {
+            // 단어 경계 → 현재 단어 뒤집고 공백 추가
+            reverse(buffer.begin(), buffer.end());
+            ret += buffer + ' ';
+            buffer.clear();
         }
         else {
-            stk.push(s[i]);
+            // 태그 밖의 단어일 때만 버퍼에 쌓기
+            buffer += c;
         }
     }
-    make();
 
-    cout << ret << "\n";
+    // 마지막 단어 flush
+    if (!buffer.empty()) {
+        reverse(buffer.begin(), buffer.end());
+        ret += buffer;
+    }
+
+    cout << ret << '\n';
     return 0;
 }
