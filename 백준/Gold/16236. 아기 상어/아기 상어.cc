86648797fedbe927cf2e3e1bcae04300f9026a21
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-11-10 20:27:52 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,72 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, -1, 0, 1};
-int n, a[24][24], visited[24][24], sss = 2, ret;
-struct fish {
-    int size, y, x;
-};
-fish shark;
-vector<fish> fish_list;
+
+const int dy[] = {-1, 0, 0, 1};
+const int dx[] = {0, -1, 1, 0};
+
+int n, a[24][24];
+int ret, eat_cnt = 0;
+
+struct Shark {
+    int y, x, size = 2;
+} shark;
+
+int bfs() {
+    queue<pair<int, int>> q;
+    int visited[24][24] = {};
+    q.push({shark.y, shark.x});
+    visited[shark.y][shark.x] = 1;
+
+    vector<pair<int,int>> candidates;
+    int min_dist = INT_MAX;
+
+    while(!q.empty()) {
+        auto [y, x] = q.front(); q.pop();
+        int dist = visited[y][x] - 1;
+        if(dist > min_dist) break;
+
+        for(int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+            if(visited[ny][nx]) continue;
+            if(a[ny][nx] > shark.size) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+
+            if(a[ny][nx] != 0 && a[ny][nx] < shark.size) {
+                min_dist = dist;
+                candidates.push_back({ny, nx});
+            }
+            q.push({ny, nx});
+        }
+    }
+
+    if(candidates.empty()) return -1;
+
+    sort(candidates.begin(), candidates.end()); // 자동으로 위→왼 정렬
+    auto [fy, fx] = candidates[0];
+    a[fy][fx] = 0;
+    ret += visited[fy][fx] - 1;
+    eat_cnt++;
+    if(eat_cnt == shark.size) {
+        shark.size++;
+        eat_cnt = 0;
+    }
+    shark.y = fy; shark.x = fx;
+    return 1;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+
     cin >> n;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++) {
             cin >> a[i][j];
             if(a[i][j] == 9) {
-                shark = {2, i, j};
+                shark.y = i;
+                shark.x = j;
+                a[i][j] = 0;
             }
-            else if(a[i][j] != 0) fish_list.push_back({a[i][j], i, j});
         }
-    }
 
-    while(1) {
-        queue<pair<int, int>> q;
-        vector<pair<int, int>> bucket;
-        q.push({shark.y, shark.x});
-        memset(visited, 0, sizeof(visited));
-        visited[shark.y][shark.x] = 1;
-        while(!q.empty()) {
-            auto [y, x] = q.front(); q.pop();
-            for(int i = 0; i < 4; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-                if(visited[ny][nx]) continue;
-                if(shark.size < a[ny][nx]) continue;
-                if(shark.size > a[ny][nx] && a[ny][nx] != 0) bucket.push_back({ny, nx});
-                q.push({ny, nx});
-                visited[ny][nx] = visited[y][x] + 1;  
-            }
-        }
-        if(!(int)bucket.size()) break;
-        else {
-            pair<int, int> next;
-            int next_dist = INT_MAX;
-            for(auto b : bucket) {
-                if(next_dist > visited[b.first][b.second] - 1) {
-                    next = {b.first, b.second};
-                    next_dist = visited[b.first][b.second] - 1;
-                }
-                else if(next_dist == visited[b.first][b.second] - 1) {
-                    if(next.first > b.first) next = b;
-                    else if(next.first == b.first && next.second > b.second) next = b; 
-                }
-            }
-            ret += next_dist;
-            sss--;
-            a[shark.y][shark.x] = 0;
-            shark = {(sss == 0 ? shark.size + 1 : shark.size), next.first, next.second};
-            a[shark.y][shark.x] = 9;
-
-            if(sss == 0) sss = shark.size;
-        }
-    }
-    
+    while(bfs() != -1);
     cout << ret << "\n";
-    
-    return 0;
 }
