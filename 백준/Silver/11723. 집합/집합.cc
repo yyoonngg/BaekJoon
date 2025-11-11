4ac6daf2c744d39ef3637ce2d1bcb01f033fa76f
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-11-11 17:13:41 +0900

    [Silver V] Title: 집합, Time: 588 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, ret;
+string s;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++) {    
+        cin >> s;
+        if(s == "add") {
+            cin >> m;
+            ret = (ret | (1 << (m-1)));
+        }
+        else if(s == "remove") {
+            cin >> m;
+            if(ret & (1 << (m-1))) ret = (ret ^ (1 << (m-1)));
+        }
+        else if(s == "check") {
+            cin >> m;
+            if(ret & (1 << (m-1))) cout << 1 << "\n";
+            else cout << 0 << "\n";
+        }
+        else if(s == "toggle") {
+            cin >> m;
+            ret = (ret ^ (1 << (m - 1)));
+        }
+        else if(s == "all") {
+            ret |= (1 << 20) - 1;
+        }
+        else if(s == "empty") {
+            ret = 0;
+        }
+    }
+    return 0;
+}
