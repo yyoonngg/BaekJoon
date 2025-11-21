161010b7c7483d59893e24c5d08a1bf2d857c630
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2025-11-22 07:07:14 +0900

    [Silver V] Title: 문서 검색, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.cc
+++ 백준/Silver/1543. 문서 검색/문서 검색.cc
@@ -1,30 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-string s1, s2;
-int ret;
+#include <stdio.h>
+#include <string.h>
+char a[2504], b[54], ret;
 
 int main() {
-    getline(cin, s1); getline(cin, s2);
+    fgets(a, sizeof(a), stdin);
+    fgets(b, sizeof(b), stdin);
+    int lena = strlen(a);
+    int lenb = strlen(b);
+    if(a[lena-1] == '\n') a[lena-1] = '\0';
+    if(b[lenb-1] == '\n') b[lenb-1] = '\0';
 
-    for(int i = 0; i < (int)s1.size() - (int)s2.size() + 1; i++) {
-        bool flag = false;
-        for(int j = 0; j < (int)s2.size(); j++) {
-            if(s1[i+j] == s2[j]) {
-                flag = true;
-            }   
-            else {
-                flag = false;
-                break;
-            } 
-        }
-        if(flag) {
-            ret++;
-            i += ((int)s2.size() - 1);
-        }
+    char *pos = a;
+    while(1) {
+        char *p = strstr(pos, b);
+        if(p == NULL) break;
+
+        ret++;
+        pos = p + lenb-1;
     }
 
-    cout << ret << "\n";
-
-
+    printf("%d", ret);
     return 0;
 }
